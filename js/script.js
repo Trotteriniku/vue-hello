682f88f6b7d2e174c0063686352baae3fbb47149
Trotteriniku <stefanozidda3@gmail.com>
2023-10-27 18:25:33 +0200

    milestone1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const {createApp}= Vue;
+
+createApp({
+    data(){
+        return {
+            message: 'Ciao Stefano,Clelia eMarco ',
+            text: 'potreste dirmi se i miei commit iniziali vanno bene ?'
+        }
+    },
+    
+}).mount('#app')
